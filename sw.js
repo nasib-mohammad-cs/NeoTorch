63b893b1e6a79b15b99531b55c3dc6a8e92747a7
Nasib Mohammad <87854710+nasib-mohammad-cs@users.noreply.github.com>
2025-07-14 14:43:28 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+// sw.js
+const CACHE_NAME = 'neotorch-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icons/icon-192.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
